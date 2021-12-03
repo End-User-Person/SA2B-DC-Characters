99508a27cacc5504ba43374d49f68f303fe75f9c
End User <josethegamer77@gmail.com>
2021-12-02 19:01:45 -0800

    Separated all relevant data in the codebase into character header files, DCCharacters.cpp contains general information now

---
+++ DCCharacters/Shadow.h
@@ -0,0 +1,34 @@
+#pragma once
+
+//End Position
+StartPosition ShadowEndingArray[] = {
+	{ LevelIDs_BasicTest, 0, 0, 0, { 0 }, { 0 }, { 0 } },
+	{ LevelIDs_GreenForest, 0x8000, 0x8000, 0x8000, { 10935, -1854, 11056 }, { 10935, -1854, 11076 }, { 10935, -1854, 11066 } },
+	{ LevelIDs_WhiteJungle, 0xC000, 0xC000, 0xC000, { 13166, -3599, -5518 }, { 13156, -3599, -5518 }, { 13176, -3599, -5518 } },
+	{ LevelIDs_SkyRail, 0x4000, 0x4000, 0x4000, { -1343.7f, -4928, 10098 }, { -1333.7f, -4928, 10098 }, { -1353.7f, -4928, 10098 } },
+	{ LevelIDs_CityEscape, 0x4000, 0x4000, 0x4000, { 3955, -22175, 16130 }, { 3955, -22175, 16130 }, { 3955, -22175, 16100 } },
+	{ LevelIDs_RadicalHighway, 0xC000, 0xC000, 0xC000, { -9970, -6999, -18430 }, { -9970, -6999, -18430 }, { -9970, -6999, -18400 } },
+	{ LevelIDs_SonicVsShadow1, 0x6C00, 0x6C00, 0x6C00, { 10, -42, 0 }, { 10, -42, 0 }, { 10, -42, 0 } },
+	{ LevelIDs_CrazyGadget, 0xC000, 0xC000, 0xC000, { -8470, -1097, -2904 }, { -8470, -1097, -2904 }, { -8470, -1097, -2904 } },
+	{ LevelIDs_HotShot, 0, 0, 0, { 0 }, { 0 }, { 0 } },
+	{ LevelIDs_Biolizard, 0, 0, 0, { -265, 10, 0 }, { -265, 10, 0 }, { -265, 10, 0 } },
+	{ LevelIDs_FinalHazard, 0, 0, 0, { 0, 0, -1000 }, { 0, 0, -1000 }, { 0, 0, -1000 } },
+	{ LevelIDs_SonicVsShadow2, 0x4000, 0x4000, 0x4000, { -292, 0, 0 }, { -292, 0, 0 }, { -292, 0, 0 } },
+	{ LevelIDs_FinalChase, 0, 0, 0, { 122, -9960, 7627 }, { 122, -9960, 7627 }, { 122, -9960, 7627 } },
+	{ LevelIDs_GreenHill, 0xC000, 0xC000, 0xC000, { -1970, -1390, -6940 }, { -1970, -1390, -6940 }, { -1970, -1390, -6910 } },
+	{ LevelIDs_FinalRush, 0, 0, 0, { 2530, -26495.5f, 7465 }, { 2530, -26495.5f, 7465 }, { 2530, -26495.5f, 7465 } },
+	{ LevelIDs_MetalHarbor2P, 0, 0x8000, 0x8000, { 8854, -31, -10370 }, { 4913, -31, -10370 }, { 4913, -31, -10370 } },
+	{ LevelIDs_GrindRace, 0x8000, 0x8000, 0x8000, { 13990, -35280, 10050 }, { 13990, -35280, 10050 }, { 13990, -35280, 10050 } },
+	{ LevelIDs_DowntownRace, 0xC000, 0xC000, 0xC000, { -1540, -6060, 8800 }, { -1540, -6060, 8800 }, { -1540, -6060, 8770 } },
+	{ LevelIDs_Invalid }
+};
+
+//End Positions for Missions 2 and 3
+LevelEndPosition ShadowMission23EndingArray[] = {
+	{ LevelIDs_BasicTest, 0, 0, 0, { 0 }, { 0 } },
+	{ LevelIDs_RadicalHighway, 0xC000, 0x8000, 0, { -40, -400, -970 }, { -6330, -4500, -8830 } },
+	{ LevelIDs_WhiteJungle, 0xC000, 0xB000, 0, { 5040, -2220, -1550 }, { 13280, -3157, -7370 } },
+	{ LevelIDs_FinalChase, 0xC000, 0xE000, 0, { 3408, -6592, 16865 }, { -695, -6957, 10275 } },
+	{ LevelIDs_SkyRail, 0x8000, 0xA000, 0, { -2411, -1782, 4260 }, { -3457, -1047, 3001 } },
+	{ LevelIDs_Invalid }
+};
